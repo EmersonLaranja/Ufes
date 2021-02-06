70769b9146d0a05d0bcd815f245b419edc6de6cd
EmersonLaranja <emersonlaranja@gmail.com>
2021-02-06 15:34:54 -0300

    done

--- Programação III/Revisão C/main.c
+++ Programação III/Revisão C/main.c
@@ -2,6 +2,41 @@
 #define MEDIA 7
 #define TAM_NOME 20
 
+Aluno **InicializaVetorAlunos(int tam)
+{
+  Aluno **vetor = (Aluno **)malloc(sizeof(Aluno *) * tam);
+  return vetor;
+};
+
+void LeArquivoEPreencheAluno(FILE *entrada, Aluno **vetor, int tam)
+{
+  int notaAuxiliar;
+  char nomeAuxiliar[TAM_NOME];
+  for (int i = 0; i < tam; i++)
+  {
+    fscanf(entrada, "%d %s", &notaAuxiliar, nomeAuxiliar);
+    vetor[i] = InicializaAluno(nomeAuxiliar, notaAuxiliar);
+  }
+};
+
+void ImprimeVetor(Aluno **vetor, int tam)
+{
+  for (int i = 0; i < tam; i++)
+  {
+    if (RetornaNota(vetor[i]) >= MEDIA)
+      ImprimeAluno(vetor[i]);
+  }
+};
+
+void LiberaVetor(Aluno **vetor, int tam)
+{
+  for (int i = 0; i < tam; i++)
+  {
+    LiberaAluno(vetor[i]);
+  }
+  free(vetor);
+};
+
 int main(int argc, char const *argv[])
 {
   FILE *entrada = fopen(argv[1], "r");
@@ -13,36 +48,23 @@ int main(int argc, char const *argv[])
   }
 
   int tamVetor;
-  fscanf(entrada, "%d", &tamVetor);
+  fscanf(entrada, "%d", &tamVetor); //Le tamanho do vetor passado na primeira linha
 
-  int notaAuxiliar;
-  char nomeAuxiliar[TAM_NOME];
+  Aluno **vetorAlunos = InicializaVetorAlunos(tamVetor); //Aloca o vetor
 
-  Aluno **vetorAlunos = (Aluno **)malloc(sizeof(Aluno *) * tamVetor);
+  LeArquivoEPreencheAluno(entrada, vetorAlunos, tamVetor); //Preenche o vetor
 
-  for (int i = 0; i < tamVetor; i++)
-  {
-    fscanf(entrada, "%d %s", &notaAuxiliar, nomeAuxiliar);
-    vetorAlunos[i] = InicializaAluno(nomeAuxiliar, notaAuxiliar);
-  }
   fclose(entrada);
 
   OrdenaNomeAlunos(vetorAlunos, tamVetor);
 
   FILE *saida = fopen("saida.csv", "w");
 
-  for (int i = 0; i < tamVetor; i++)
-  {
-    if (RetornaNota(vetorAlunos[i]) >= MEDIA)
-      ImprimeAluno(vetorAlunos[i]);
-  }
+  ImprimeVetor(vetorAlunos, tamVetor); //Imprime o vetor
+
   fclose(saida);
 
-  for (int i = 0; i < tamVetor; i++)
-  {
-    LiberaAluno(vetorAlunos[i]);
-  }
-  free(vetorAlunos);
+  LiberaVetor(vetorAlunos, tamVetor); //libera o vetor
 
   return (0);
 }
